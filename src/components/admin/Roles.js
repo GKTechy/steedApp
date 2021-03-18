5e5e9e869f020396792f0bae613f793e2bcb4e1a
Glapskrish <glapskrish@gmail.com>
2021-03-18 08:35:31 +0530

    added the roles events

--- src/components/admin/Roles.js
+++ src/components/admin/Roles.js
@@ -13,59 +13,11 @@ export class Roles extends Component {
     
         this.state = {
             rolename:"",
+            roleid:0,
+            active:true,
+            showModal:false,
             errormsg:"",
             rolesList:[
-
-                {
-                    id:1,
-                    role:"admin",
-                    status:'Active'
-                }, {
-                    id:2,
-                    role:"root",
-                    status:'Active'
-                }, {
-                    id:3,
-                    role:"qc",
-                    status:'Active'
-                }, {
-                    id:4,
-                    role:"sales",
-                    status:'Active'
-                }, {
-                    id:5,
-                    role:"test",
-                    status:'Active'
-                }, {
-                    id:6,
-                    role:"purchase",
-                    status:'Active'
-                }, {
-                    id:7,
-                    role:"Manufacturing",
-                    status:'Active'
-                }, {
-                    id:8,
-                    role:"Accounting",
-                    status:'Active'
-                }, {
-                    id:9,
-                    role:"develop",
-                    status:'Active'
-                }, {
-                    id:10,
-                    role:"admin1",
-                    status:'Active'
-                }, {
-                    id:11,
-                    role:"admin2",
-                    status:'Active'
-                }, {
-                    id:12,
-                    role:"admin3",
-                    status:'Active'
-                },
-                
             ]
         }
        
@@ -86,15 +38,22 @@ export class Roles extends Component {
             this.setState({
                 errormsg: "Enter Role"
             });
+        }else if(!this.state.active && this.state.roleid===0){
+            this.setState({
+                errormsg: "Select Active"
+            });
         }else{
-            const obj = {'role':this.state.rolename, status:'Active','id':1*15};
+            const obj = {'rolename':this.state.rolename, active:this.state.active,'roleid':this.state.roleid};
+          //  console.log("c role->"+obj)
             this.setState({
                 errormsg: "",
                 rolesList: [...this.state.rolesList, obj],
-                rolename:""
+                rolename:"",
+                roleid:0,
+                active:true
             },()=>{
                 $('#myTable').DataTable();
-                console.log(this.state.rolesList); 
+              //  console.log(this.state.rolesList); 
             });
         }
      }
@@ -106,6 +65,36 @@ export class Roles extends Component {
         });
     };
 
+    handleCheckClick = () => {
+        this.setState({ active: !this.state.active });
+    }
+
+    editEvent = (obj) => {
+        console.log("-->"+JSON.stringify(obj))
+        this.setState({
+            rolename:obj.rolename,
+            roleid:obj.rolename,
+            active:obj.active
+        });
+
+    }
+    
+    deleteAlertEvent = (obj) => {
+      //  alert("id-->"+id)
+        this.setState({
+            rolename:obj.rolename,
+            roleid:obj.rolename,
+            active:obj.active,
+            showModal:true,
+        });
+    }
+
+    deleteEvent= () => {
+       alert("Deleted!!");
+       this.setState({
+        showModal:false,
+    });
+    }
     
     render() {
         return (
@@ -114,18 +103,26 @@ export class Roles extends Component {
                 <div className="content">
                     <div className="container-fluid">
                         <div className="row">
-                        <div className="col-12">
-                              
-                        <div className="input-group input-group-sm col-4">
-                            <input type="text" 
-                            className='form-control'
-                             placeholder="Enter Role..." name="rolename" id="rolename" value= {this.state.rolename} onChange={this.handleFormChange}/>
-                            <span className="input-group-append">
+                            <div className="col-3">
+                                <input type="text" className="form-control" placeholder="rolename" name="rolename" id="rolename" value= {this.state.rolename} onChange={this.handleFormChange}/>
+                                
+                            </div>
+                            <div className="col-1">
+                                <div className="form-group p-2  ms-2">&nbsp;&nbsp;
+                                        <input className="form-check-input" type="checkbox" checked={this.state.active}  onChange={this.handleCheckClick}/><label className="form-check-label">Active</label>
+                                </div>
+                            </div>
+                            <div className="col-1">
                                 <button type="button" className="btn btn-primary btn-flat" onClick={this.addRole}>Add</button>
-                            </span>
+                            </div>
+                            <div className="col-2">
+                            <span className="text-danger">{this.state.errormsg}</span>
+                            </div>
                         </div>
-                        <span className="text-danger">{this.state.errormsg}</span>
-                        <br/>
+
+
+                        <div className="row">
+                        <div className="col-12">
                             <div className="table-responsive-sm" >
                                 <table className="table table-striped table-sm table-bordered" id="myTable" >
                                     <thead>
@@ -139,15 +136,18 @@ export class Roles extends Component {
                                     <tbody>
 
                                         {
-                                            this.state.rolesList.map((item) =>
+                                            this.state.rolesList.map((obj) =>
                                             
-                                            <tr key={item.id}>
+                                            <tr key={obj.roleid}>
                                                 
-                                                <td>{item.role}</td>
-                                                <td><span className="tag tag-success">{item.status}</span></td>
+                                                <td>{obj.rolename}</td>
+                                                <td>{
+                                                        obj.active ? <span className="tag tag-success">Active</span> : <span className="tag tag-failure">In Active</span>
+                                                    }
+                                                </td>
                                                 <td className="project-actions ">
-                                                    <a className="btn btn-info btn-sm" href="#"><i className="fas fa-pencil-alt"></i>Edit</a>&nbsp;&nbsp;
-                                                    <a className="btn btn-danger btn-sm" href="#" data-toggle="modal" data-target="#modal-default"><i className="fas fa-trash"></i>Delete</a>
+                                                    <a className="btn btn-info btn-sm" href="#" onClick={ ()=> this.editEvent(obj)}><i className="fas fa-pencil-alt"></i>Edit</a>&nbsp;&nbsp;
+                                                    <a className="btn btn-danger btn-sm" href="#" data-toggle="modal" data-target="#modal-default" onClick={ ()=> this.deleteAlertEvent(obj)}><i className="fas fa-trash"></i>Delete</a>
                                                 </td>
                                             </tr>
                                         )}
@@ -163,7 +163,7 @@ export class Roles extends Component {
                     </div>
                     </div>
 
-                   <div className="modal fade" id="modal-default">
+                   <div className="modal fade" id="modal-default" show={this.state.showModal}>
                     <div className="modal-dialog">
                         <div className="modal-content">
                         <div className="modal-header">
@@ -173,11 +173,11 @@ export class Roles extends Component {
                             </button>
                         </div>
                         <div className="modal-body">
-                            <p>Do you Want to Delete?</p>
+                            <p>Do you Want to Delete Role: {this.state.rolename} ?</p>
                         </div>
                         <div className="modal-footer justify-content-between">
                             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Delete</button>
+                            <button type="button" className="btn btn-primary" onClick={ ()=> this.deleteEvent()}>Delete</button>
                         </div>
                         </div>
                     </div>
