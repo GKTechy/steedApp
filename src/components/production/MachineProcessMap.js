05ff844a7970e12f9fd5282502ad340ddaa5e010
Glapskrish <glapskrish@gmail.com>
2021-03-30 07:39:13 +0530

    machine Process Map completed

--- src/components/production/MachineProcessMap.js
+++ src/components/production/MachineProcessMap.js
@@ -15,15 +15,15 @@ export class MachineProcessMap extends Component {
                 sortable: true
             },
             {
-                key: "Process",
+                key: "processName",
                 text: "Process",
                 sortable: true
             },
-            {
-                key: "isActive",
-                text: "Active",
-                sortable: true
-            },
+            // {
+            //     key: "isActive",
+            //     text: "Active",
+            //     sortable: true
+            // },
             {
                 key: "action",
                 text: "Action",
@@ -32,7 +32,7 @@ export class MachineProcessMap extends Component {
                             <button
                                 className="btn btn-info btn-sm"
                                 onClick={this.editRecord.bind(this, record, index)}
-                                style={{marginRight: '5px'}} data-toggle="modal" data-target="#new-machine">
+                                style={{marginRight: '5px'}} >
                                     <i className="fas fa-pencil-alt" ></i>Edit
                             </button>
                        
@@ -42,13 +42,13 @@ export class MachineProcessMap extends Component {
         ];
 
         this.config = {
-            key_column: 'machineId',
+            key_column: 'machineprocessMapId',
             page_size: 10,
             length_menu: [10, 20, 50],
             show_filter: true,
             show_pagination: true,
             pagination: 'advance',
-            filename: "Machine",
+            filename: "Machine Process Map",
             button: {
                 excel: true,
                 print: true,
@@ -58,6 +58,7 @@ export class MachineProcessMap extends Component {
 
 
         this.state = {
+            machineprocessMapId:0,
             machineId:"",
             processId:"",
             active:true,
@@ -77,11 +78,11 @@ export class MachineProcessMap extends Component {
      }  
 
      getTableValues(){
-        console.log('MachineProcessMap props profile-->:'+this.props.apiurl)
+    //    console.log('MachineProcessMap props profile-->:'+this.props.apiurl)
         fetch(this.props.apiurl+"machine/allMachinesProcessMapDetails")
         .then(res => res.json())
         .then( (result) => {
-                console.log("result-->"+JSON.stringify(result))
+               // console.log("result-->"+JSON.stringify(result))
                 if(result.valid){
                     this.setState({
                         records: result.machineProcessMapList,
@@ -96,15 +97,111 @@ export class MachineProcessMap extends Component {
     }
 
     handleChange = (event) =>{
-        this.setState({machineId: event.target.value});
+        this.setState({[event.target.name]: event.target.value});
     }
     
+    editRecord = (record, index) => {
+        // console.log("Edit record", index, record);
+        // console.log("-->"+JSON.stringify(record))
+        // var tempstatus=true;
+        // if(record.isActive === "Active"){
+        //      tempstatus=true;
+        //  }else{
+        //      tempstatus=false;
+        //  }
+         this.setState({
+            machineprocessMapId:record.machineprocessMapId,
+            machineId:record.machineNameId,
+            processId:record.processId,
+         });
+     }
+ 
+     resetClick  = () =>{
+        this.setState({
+            machineprocessMapId:0,
+            machineId:"",
+            processId:"",
+         });
+    }
+    saveClick= event =>{
+
+     //   console.log("state values-->"+JSON.stringify(this.state))
+        if(this.state.machineId === "0"){
+            this.setState({
+                errormsg: "Select Machine"
+            });
+        }else if(this.state.processId === "0"){
+            this.setState({
+                errormsg: "Select Process"
+            });
+        }
+        // else if(!this.state.isActive && this.state.paymentTermsId===0){
+        //     this.setState({
+        //         errormsg: "Select Active"
+        //     });
+        // }
+        else{
+            // var tempstatus="";
+            // if(this.state.isActive){
+            //     tempstatus="Active"
+            // }else{
+            //     tempstatus="InActive"
+            // }
+            
+             const obj = { machineprocessMapId:this.state.machineprocessMapId,'machineNameId':this.state.machineId,'processId':this.state.processId, "updatedBy":this.state.loginUser.userId,"createdBy":this.state.loginUser.userId};
+
+            //  // POST request using fetch with error handling
+                 const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ "machineProcessMapObj": obj })
+                };
+                fetch(this.props.apiurl+"machine/saveMachinesProcessMapDetails", requestOptions)
+                    .then(async response => {
+                        const data = await response.json();
+                        console.log("--data--"+JSON.stringify(data))
+                        // check for error response
+                        if (!response.ok) {
+                            // get error message from body or default to response status
+                            const error = (data && data.message) || response.status;
+                            return Promise.reject(error);
+                        }
+
+                        if(data.valid){
+                             //  console.log("c role->"+obj)
+                             this.setState({
+                                errormsg: "",
+                                records: data.machineProcessMapList,
+                                machineId:0,
+                                processId:0,
+                                //isActive:true
+                            },()=>{});
+                        }else{
+                            this.setState({ errormsg: data.responseMsg});
+                            this.setState({
+                                errormsg: "",
+                                machineId:0,
+                                processId:0,
+                                //isActive:true
+                            },()=>{});
+                        }
+                           
+                    })
+                    .catch(error => {
+                        this.setState({ errormsg: error.toString() });
+                      //  console.error('There was an error!', error);
+                    });
+                    
+          
+        }
+     }
+
 
     render() {
         let optionTemplate = this.state.processList.map(v => (
             <option value={v.processId}>{v.processName}</option>
           ));
-        console.log('optionTemplate-->'+optionTemplate)
+       // console.log('optionTemplate-->'+optionTemplate)
         return (
             <div>
                  <section className="content">
@@ -116,7 +213,8 @@ export class MachineProcessMap extends Component {
                             <div className="card-header form-inline">
                                 <div className="form-group col-md-3">
                                     <label htmlFor="exampleSelectRounded0">Machine Name</label>&nbsp;&nbsp;&nbsp;
-                                    <select className="custom-select custom-select-sm rounded-0"  id="machineId" value={this.state.machineId} onChange={this.change} >
+                                    <select className="custom-select custom-select-sm rounded-0"  id="machineId" name="machineId" value={this.state.machineId} onChange={this.handleChange} >
+                                        <option value="0"></option>
                                         {this.state.machineList.map(o => (
                                             <option value={o.machineId}>{o.machineName}</option>
                                         ))}
@@ -124,13 +222,15 @@ export class MachineProcessMap extends Component {
                                 </div>
                                 <div className="form-group col-md-3">
                                     <label htmlFor="exampleSelectRounded0">Process Name</label>&nbsp;&nbsp;&nbsp;
-                                    <select className="custom-select  custom-select-sm rounded-0" id="processId" value={this.state.processId} onChange={this.change} >
+                                    <select className="custom-select  custom-select-sm rounded-0" id="processId" name="processId" value={this.state.processId} onChange={this.handleChange} >
+                                        <option value="0"></option>
                                          {optionTemplate}
                                     </select>
                                 </div>
-                                <div className="col-md-1">
+                                <div className="col-md-3">
                                          
                                         <button type="button" className="btn btn-primary" id="save" onClick={this.saveClick}>Save</button>&nbsp;&nbsp;&nbsp;
+                                        <button type="button" className="btn btn-primary" id="save" onClick={this.resetClick}>Clear</button>&nbsp;&nbsp;&nbsp;
                                 </div>
 
                             
